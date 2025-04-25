bbe29b27791cce7419253a0884dafbe65500698e
SirMarkus <marcosbarreroherrero@gmail.com>
2025-04-26 00:52:19 +0200

    style(root): Add biome as linter (issue fixes #6)

--- src/server/controllers/products.controller.ts
+++ src/server/controllers/products.controller.ts
@@ -1,23 +1,22 @@
 import { ProductsModel } from "../models/products.model"
 
 export class ProductsController {
-    productsModel = new ProductsModel
+  productsModel = new ProductsModel()
 
-    getAll() {
-        return this.productsModel.getAll() 
+  getAll() {
+    return this.productsModel.getAll()
+  }
+
+  getById(id: number) {
+    try {
+      const product = this.productsModel.getById(id)
+      return product
+    } catch {
+      throw new Error("404, Product not found.")
     }
+  }
 
-    getById(id: number) {
-        
-        try {
-            const product = this.productsModel.getById(id)
-            return product
-        } catch {
-            throw new Error("404, Product not found.")
-        }
-    }
-
-    post(name: string, price: number) {
-        return this.productsModel.insert(name, price)
-    } 
+  post(name: string, price: number) {
+    return this.productsModel.insert(name, price)
+  }
 }
--- src/server/routes/products.ts
+++ src/server/routes/products.ts
@@ -1,68 +1,66 @@
-import {  Router } from "express";
+import { Router } from "express"
 
-import { ProductsController } from "../controllers/products.controller";
+import { ProductsController } from "../controllers/products.controller"
 
 export const productsRouter = Router()
 
-
-const productsController = new ProductsController
+const productsController = new ProductsController()
 
 productsRouter.get("/", (_, res) => {
-    res.json({
-        products: productsController.getAll(),
-        responseCode: 200
-    })
+  res.json({
+    products: productsController.getAll(),
+    responseCode: 200,
+  })
 })
 
 productsRouter.get("/:id", (req, res) => {
-    const id = Number(req.params.id)
+  const id = Number(req.params.id)
 
-    console.log(id)
+  console.log(id)
 
-    if (isNaN(id)) {
-        res.status(500).json({
-            message: "Id is not a valid number.",
-            products: [],
-            responseCode: 500
-        })
-        return
-    }
+  if (Number.isNaN(id)) {
+    res.status(500).json({
+      message: "Id is not a valid number.",
+      products: [],
+      responseCode: 500,
+    })
+    return
+  }
 
-    try {
-        const product = productsController.getById(id)
-        
-        res.json({
-            products: [product],
-            response: 200
-        })
+  try {
+    const product = productsController.getById(id)
 
-    } catch {
-        res.status(404).json({
-            message: "Product not found",
-            products: [],
-            responseCode: 404
-        })
-    }
+    res.json({
+      products: [product],
+      response: 200,
+    })
+  } catch {
+    res.status(404).json({
+      message: "Product not found",
+      products: [],
+      responseCode: 404,
+    })
+  }
 })
 
 productsRouter.post("/", (req, res) => {
-    const name = req.body.name
-    const price = req.body.price
+  const name = req.body.name
+  const price = req.body.price
 
-    if (name === undefined || price === undefined) {
-        res.status(500).json({
-            message: "Invalid body",
-            products: [],
-            responseCode: 500
-        })
-
-        return
-    }
-
-    const postedProduct = productsController.post(name, price)
-
-    res.status(201).json({
-        products: [postedProduct],
-        responseCode: 201
+  if (name === undefined || price === undefined) {
+    res.status(500).json({
+      message: "Invalid body",
+      products: [],
+      responseCode: 500,
     })
+
+    return
+  }
+
+  const postedProduct = productsController.post(name, price)
+
+  res.status(201).json({
+    products: [postedProduct],
+    responseCode: 201,
+  })
 })
