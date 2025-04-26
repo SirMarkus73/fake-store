d40f9079bce4ca53009e11da68ca26f8ae5960b4
SirMarkus <marcosbarreroherrero@gmail.com>
2025-04-26 17:19:26 +0200

    refactor(server): Use controllers directly on routes (issue fixes #16)

--- src/server/controllers/products.controller.ts
+++ src/server/controllers/products.controller.ts
@@ -1,22 +1,72 @@
+import { z } from "zod"
 import { ProductsModel } from "@models/products.model"
+import type { Request, Response } from "express"
 
 export class ProductsController {
   productsModel = new ProductsModel()
 
-  getAll() {
-    return this.productsModel.getAll()
+  getAll = (_: Request, res: Response) => {
+    const products = this.productsModel.getAll()
+
+    res.status(200).json({
+      products,
+      responseCode: 200,
+    })
   }
 
-  getById(id: number) {
+  getById = (req: Request, res: Response) => {
+    const id = req.params.id
+
+    const parsedId = z.coerce.number().safeParse(id)
+
+    if (!parsedId.success) {
+      res.status(500).json({
+        message: "Id is not a valid number or missing.",
+        products: [],
+        responseCode: 500,
+      })
+      return
+    }
+
     try {
-      const product = this.productsModel.getById(id)
-      return product
+      const product = this.productsModel.getById(parsedId.data)
+
+      res.status(200).json({
+        products: [product],
+        response: 200,
+      })
     } catch {
-      throw new Error("404, Product not found.")
+      res.status(404).json({
+        message: "Product not found.",
+        products: [],
+        responseCode: 404,
+      })
     }
   }
 
-  post(name: string, price: number) {
-    return this.productsModel.insert(name, price)
+  post = (req: Request, res: Response) => {
+    const bodySchema = z.object({
+      name: z.string(),
+      price: z.number(),
+    })
+
+    const parsedBody = bodySchema.safeParse(req.body)
+
+    if (!parsedBody.success) {
+      res.status(500).json({
+        message: "Name or price is not valid or missing.",
+        products: [],
+        responseCode: 500,
+      })
+      return
+    }
+
+    const { name, price } = parsedBody.data
+    const postedProduct = this.productsModel.insert(name, price)
+
+    res.status(201).json({
+      products: [postedProduct],
+      responseCode: 201,
+    })
   }
 }
