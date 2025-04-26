ea1593e5cad9c59967e1ee64c16abeb04b6cd546
SirMarkus <marcosbarreroherrero@gmail.com>
2025-04-26 20:11:21 +0200

    feat(server): Modify controllers and models to use the db (issue fixes #11)

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -11,8 +11,8 @@ export const product = sqliteTable(
   "product",
   {
     id: integer().primaryKey({ autoIncrement: true }),
-    name: text(),
-    price: numeric(),
+    name: text().notNull(),
+    price: numeric({ mode: "number" }).notNull(),
   },
   (table) => [check("price_check", sql`${table.price} > 0`)],
 )
--- src/server/models/products.model.ts
+++ src/server/models/products.model.ts
@@ -1,40 +1,32 @@
-export class ProductsModel {
-  #products = [
-    {
-      id: 1,
-      name: "Watch",
-      price: 120.4,
-    },
-    {
-      id: 2,
-      name: "T-Shirt",
-      price: 11.99,
-    },
-    {
-      id: 3,
-      name: "Jeans",
-      price: 13.54,
-    },
-  ]
+import { product } from "@/db/schema"
+import { db } from "@/db/connection"
+import { eq } from "drizzle-orm"
 
-  getAll() {
-    return this.#products
+export class ProductsModel {
+  getAll = async () => {
+    const products = await db.select().from(product)
+    return products
   }
 
-  getById(id: number) {
-    const selectedProduct = this.#products.find((product) => product.id === id)
+  getById = async (id: number) => {
+    const selectedProduct = await db
+      .select()
+      .from(product)
+      .where(eq(product.id, id))
 
-    if (selectedProduct === undefined) {
+    if (selectedProduct.length === 0) {
       throw new Error("Product not found")
     }
 
-    return selectedProduct
+    return selectedProduct[0]
   }
 
-  insert(name: string, price: number) {
-    const lastId = this.#products[this.#products.length - 1].id
-    this.#products.push({ id: lastId + 1, name, price })
+  insert = async (name: string, price: number) => {
+    const productInserted = await db
+      .insert(product)
+      .values({ name, price })
+      .returning()
 
-    return this.#products[this.#products.length - 1]
+    return productInserted
   }
 }
