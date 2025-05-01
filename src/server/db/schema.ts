2d04f541ac65cbc26c0843db0067886fa805b01d
SirMarkus <marcosbarreroherrero@gmail.com>
2025-05-01 22:15:09 +0200

    feat(server): Add new tables for the category system (issue #22)

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -5,6 +5,7 @@ import {
   numeric,
   sqliteTable,
   text,
+  primaryKey,
 } from "drizzle-orm/sqlite-core"
 
 export const product = sqliteTable(
@@ -15,4 +16,24 @@ export const product = sqliteTable(
     price: numeric({ mode: "number" }).notNull(),
   },
   (table) => [check("price_check", sql`${table.price} > 0`)],
+)
+
+export const category = sqliteTable("category", {
+  id: integer().primaryKey({ autoIncrement: true }),
+  name: text().notNull(),
+  description: text().notNull(),
+})
+
+export const productCategory = sqliteTable(
+  "product_category",
+  {
+    productId: integer().references(() => product.id),
+    categoryId: integer().references(() => category.id),
+  },
+  (table) => [
+    primaryKey({
+      name: "pk_product_category",
+      columns: [table.categoryId, table.productId],
+    }),
+  ],
 )
