e7d787e0caf51aa4921c527542d510373391868e
SirMarkus <marcosbarreroherrero@gmail.com>
2025-05-05 19:19:11 +0200

    refactor(server, category): Move category types to his own files (issue #56)

---
+++ packages/server/src/types/categories.model.d.ts
@@ -0,0 +1,24 @@
+import type { DatabaseError } from "@/errors/databaseError"
+import type { Category } from "@/types/category"
+import type { PromiseResult } from "@/types/promiseResult"
+
+export type GetAllResult = PromiseResult<Category[], DatabaseError>
+export type GetByIdResult = PromiseResult<
+  Category[],
+  DatabaseError | NotFoundError
+>
+export type PostResult = PromiseResult<Category[], DatabaseError>
+export type DeleteResult = PromiseResult<Category[], DatabaseError>
+
+export interface GetByIdParams {
+  id: number
+}
+
+export interface PostParams {
+  name: string
+  description: string
+}
+
+export interface DeleteParams {
+  id: number
+}
