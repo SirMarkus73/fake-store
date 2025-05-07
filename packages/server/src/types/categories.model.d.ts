c62cbbd5491d3f0bdb005602cdc3ec2d7d242f9d
SirMarkus <marcosbarreroherrero@gmail.com>
2025-05-07 13:54:47 +0200

    style(root): Run -> bun run check (issue #59)

--- packages/server/src/types/categories.model.d.ts
+++ packages/server/src/types/categories.model.d.ts
@@ -5,9 +5,15 @@ import type { Category } from "@/types/category"
 import type { PromiseResult } from "@/types/promiseResult"
 
 export type GetAllResult = PromiseResult<Category[], DatabaseError>
-export type GetByIdResult = PromiseResult<Category[],DatabaseError | NotFoundError>
+export type GetByIdResult = PromiseResult<
+  Category[],
+  DatabaseError | NotFoundError
+>
 export type PostResult = PromiseResult<Category[], DatabaseError>
-export type PatchResult = PromiseResult<Category[],DatabaseError | ParameterError>
+export type PatchResult = PromiseResult<
+  Category[],
+  DatabaseError | ParameterError
+>
 export type DeleteResult = PromiseResult<Category[], DatabaseError>
 
 export interface GetByIdParams {
