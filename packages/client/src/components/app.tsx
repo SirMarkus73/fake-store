85ca2987f773acbc6195e56bb39eb1a4ad2b62eb
SirMarkus <marcosbarreroherrero@gmail.com>
2025-05-17 18:33:44 +0200

    refactor(client): Error Handling and refactor of components

--- packages/client/src/components/app.tsx
+++ packages/client/src/components/app.tsx
@@ -1,14 +1,9 @@
 import { tsr } from "../reactQuery"
 import { Button } from "./button"
-import { Product, ProductSkeleton } from "./product"
+import { ProductList } from "./productList"
 
 export function App() {
-  const {
-    data: products,
-    isLoading,
-    isFetching,
-    refetch,
-  } = tsr.products.getAll.useQuery({
+  const { isFetching, refetch } = tsr.products.getAll.useQuery({
     queryKey: ["products"],
     refetchOnWindowFocus: false,
   })
@@ -16,22 +11,7 @@ export function App() {
   return (
     <main className="grid min-h-dvh place-items-center bg-gradient-to-br from-gray-300 to-gray-400">
       <section className="flex flex-wrap gap-4">
-        {!isLoading &&
-          products &&
-          products.body.products.length > 0 &&
-          products.body.products.map((product) => (
-            <Product key={product.id} product={product} />
-          ))}
-        {isLoading && (
-          <>
-            <ProductSkeleton />
-            <ProductSkeleton />
-            <ProductSkeleton />
-          </>
-        )}
-        {!isLoading && products?.body.products.length === 0 && (
-          <p>No products currently 😒</p>
-        )}
+        <ProductList />
       </section>
       <Button disabled={isFetching} onClick={() => refetch()} />
     </main>
---
+++ packages/client/src/components/productList.tsx
@@ -0,0 +1,69 @@
+import {
+  exhaustiveGuard,
+  isFetchError,
+  isUnknownErrorResponse,
+} from "@ts-rest/react-query/v5"
+import { tsr } from "../reactQuery"
+import { Product, ProductSkeleton } from "./product"
+
+export function ProductList() {
+  const {
+    data: products,
+    isLoading,
+    isError,
+    error,
+    contractEndpoint,
+  } = tsr.products.getAll.useQuery({
+    queryKey: ["products"],
+    refetchOnWindowFocus: false,
+  })
+
+  if (isError) {
+    if (isFetchError(error)) {
+      return (
+        <div>
+          ❌ We could not retrieve this post. Please check your internet
+          connection. ❌
+        </div>
+      )
+    }
+
+    if (isUnknownErrorResponse(error, contractEndpoint)) {
+      return <p>❌ An unexpected error occurred, please try again later ❌</p>
+    }
+
+    if (error.status === 500) {
+      return (
+        <p>
+          ❌ There was an error getting your products, please try again later ❌
+        </p>
+      )
+    }
+
+    // this should be unreachable code if you handle all possible error cases
+    // if not, you will get a compile-time error on the line below
+    return exhaustiveGuard(error)
+  }
+
+  if (isLoading) {
+    return (
+      <>
+        <ProductSkeleton />
+        <ProductSkeleton />
+        <ProductSkeleton />
+      </>
+    )
+  }
+
+  if (products && products.body.products.length > 0) {
+    return (
+      <>
+        {products.body.products.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </>
+    )
+  }
+
+  return <p>No products currently 😒</p>
+}
