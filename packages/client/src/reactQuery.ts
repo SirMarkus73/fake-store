d390a6f940c10f950b3d0e147911880afd117883
SirMarkus <marcosbarreroherrero@gmail.com>
2025-05-18 17:32:59 +0200

    fix(client): Add window.location.origin instead of the url hardcoded

--- packages/client/src/reactQuery.ts
+++ packages/client/src/reactQuery.ts
@@ -2,7 +2,7 @@ import { initTsrReactQuery } from "@ts-rest/react-query/v5"
 import { allContract } from "../../common/contracts/all"
 
 export const tsr = initTsrReactQuery(allContract, {
-  baseUrl: "http://localhost:5173",
+  baseUrl: window.location.origin,
   baseHeaders: {
     "x-app-source": "ts-rest",
   },
